9582a078ab74f628cc0133ea8a491dc11f4465d0
Roberto Araujo <roberto.andrioli@gmail.com>
2014-01-08 19:38:10 -0200

    DefUseAnalyzer now computes frames before (Frame<Value>) and after (DefUseFrame) each instruction.
    Each DefUseFrame should contain a set of Definitions and Use of Variables

--- src/main/java/br/com/ooboo/asm/defuse/DefUseAnalyzer.java
+++ src/main/java/br/com/ooboo/asm/defuse/DefUseAnalyzer.java
@@ -1,22 +1,53 @@
 package br.com.ooboo.asm.defuse;
 
+import org.objectweb.asm.tree.AbstractInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 import org.objectweb.asm.tree.analysis.Analyzer;
+import org.objectweb.asm.tree.analysis.AnalyzerException;
 import org.objectweb.asm.tree.analysis.Frame;
 
 public class DefUseAnalyzer extends Analyzer<Value> {
 
+	private final DefUseInterpreter interpreter;
+
+	private DefUseFrame[] duframes;
+
 	public DefUseAnalyzer() {
-		super(new DefUseInterpreter());
+		this(new DefUseInterpreter());
+	}
+
+	private DefUseAnalyzer(final DefUseInterpreter interpreter) {
+		super(interpreter);
+		this.interpreter = interpreter;
 	}
 
 	@Override
-	protected Frame<Value> newFrame(final int nLocals, final int nStack) {
-		return new DefUseFrame(nLocals, nStack);
+	public Frame<Value>[] analyze(final String owner, final MethodNode m) throws AnalyzerException {
+
+		final Frame<Value>[] frames = super.analyze(owner, m);
+		final DefUseFrame[] duframes = new DefUseFrame[frames.length];
+
+		AbstractInsnNode insn;
+
+		for (int i = 0; i < frames.length; i++) {
+			duframes[i] = new DefUseFrame(frames[i]);
+			insn = m.instructions.get(i);
+			switch (insn.getType()) {
+			case AbstractInsnNode.LABEL:
+			case AbstractInsnNode.LINE:
+			case AbstractInsnNode.FRAME:
+				break;
+			default:
+				duframes[i].execute(m.instructions.get(i), interpreter);
+				break;
+			}
+		}
+
+		this.duframes = duframes;
+		return frames;
 	}
 
-	@Override
-	protected Frame<Value> newFrame(final Frame<? extends Value> src) {
-		return new DefUseFrame(src);
+	public DefUseFrame[] getDefUseFrames() {
+		return duframes;
 	}
-
 }
