f179ef1ea4656ffe2598fbc7c9431f82d8f56803
Roberto Araujo <roberto.andrioli@gmail.com>
2014-01-23 14:38:16 -0200

    Simple refactoring

--- src/main/java/br/com/ooboo/asm/defuse/DefUseAnalyzer.java
+++ src/main/java/br/com/ooboo/asm/defuse/DefUseAnalyzer.java
@@ -128,20 +128,9 @@ public class DefUseAnalyzer extends Analyzer<Value> {
 				}
 			}
 		}
-		for (int i = 0; i < nargs; i++) {
+		for (int i = 0; i < variables.length; i++) {
 			def = variables[i];
-			rdSets[0].gen(0, def);
-			for (int j = 1; j < n; j++) {
-				other = duframes[j].getDefinition();
-				if (def.equals(other)) {
-					rdSets[0].kill(j, def);
-					rdSets[j].kill(i, def);
-				}
-			}
-		}
-		for (int i = nargs; i < variables.length; i++) {
-			def = variables[i];
-			if (def instanceof StaticField) {
+			if (i < nargs || def instanceof StaticField) {
 				rdSets[0].gen(0, def);
 				for (int j = 1; j < n; j++) {
 					other = duframes[j].getDefinition();
