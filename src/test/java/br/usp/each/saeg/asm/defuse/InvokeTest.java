2878d37dbe80554f94128a3d723e848d0f4e6498
Roberto Araujo <roberto.andrioli@gmail.com>
2014-12-31 17:02:52 -0200

    Happy new year!

--- src/test/java/br/usp/each/saeg/asm/defuse/InvokeTest.java
+++ src/test/java/br/usp/each/saeg/asm/defuse/InvokeTest.java
@@ -1,6 +1,6 @@
 /**
  * asm-defuse: asm powered by definitions/uses analysis
- * Copyright (c) 2014 Roberto Araujo (roberto.andrioli@gmail.com)
+ * Copyright (c) 2014, 2015 Roberto Araujo (roberto.andrioli@gmail.com)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
