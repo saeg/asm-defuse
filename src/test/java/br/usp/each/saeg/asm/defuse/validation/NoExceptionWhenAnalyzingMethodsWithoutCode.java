6eea0ad161831cb922e3716b265398a7839b0ba6
Roberto Araujo <roberto.andrioli@gmail.com>
2016-04-02 15:54:07 -0300

    Added simple tests

---
+++ src/test/java/br/usp/each/saeg/asm/defuse/validation/NoExceptionWhenAnalyzingMethodsWithoutCode.java
@@ -0,0 +1,81 @@
+/**
+ * asm-defuse: asm powered by definitions/uses analysis
+ * Copyright (c) 2013, 2016 Roberto Araujo (roberto.andrioli@gmail.com)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package br.usp.each.saeg.asm.defuse.validation;
+
+import static br.usp.each.saeg.asm.defuse.validation.ValidationTestUtil.getClassNode;
+import static br.usp.each.saeg.asm.defuse.validation.ValidationTestUtil.getMethodNode;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.analysis.AnalyzerException;
+import org.objectweb.asm.tree.analysis.BasicInterpreter;
+import org.objectweb.asm.tree.analysis.BasicValue;
+
+import br.usp.each.saeg.asm.defuse.DefUseAnalyzer;
+import br.usp.each.saeg.asm.defuse.FlowAnalyzer;
+import br.usp.each.saeg.asm.defuse.validation.targets.Interface;
+import br.usp.each.saeg.asm.defuse.validation.targets.Native;
+
+@RunWith(Parameterized.class)
+public class NoExceptionWhenAnalyzingMethodsWithoutCode {
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { Interface.class, "interfaceMethod", "()V" },
+                { Native.class, "nativeMethod", "()V" }
+        });
+    }
+
+    private final MethodNode mn;
+
+    public NoExceptionWhenAnalyzingMethodsWithoutCode(
+            final Class<?> target, final String name, final String desc) throws IOException {
+        mn = getMethodNode(getClassNode(target), name, desc);
+    }
+
+    @Test
+    public void flowAnalyzer() throws AnalyzerException {
+        new FlowAnalyzer<BasicValue>(new BasicInterpreter()).analyze("owner", mn);
+    }
+
+    @Test
+    public void defUseAnalyzer() throws AnalyzerException {
+        new DefUseAnalyzer().analyze("owner", mn);
+    }
+
+}
---
+++ src/test/java/br/usp/each/saeg/asm/defuse/validation/ValidationTestUtil.java
@@ -0,0 +1,63 @@
+/**
+ * asm-defuse: asm powered by definitions/uses analysis
+ * Copyright (c) 2013, 2016 Roberto Araujo (roberto.andrioli@gmail.com)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holders nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package br.usp.each.saeg.asm.defuse.validation;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.NoSuchElementException;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+
+public class ValidationTestUtil {
+
+    public static ClassNode getClassNode(final Class<?> target) throws IOException {
+        final String file = "/" + target.getName().replace('.', '/') + ".class";
+        final InputStream is = ValidationTestUtil.class.getResourceAsStream(file);
+        final ClassReader classReader = new ClassReader(is);
+        final ClassNode cn = new ClassNode(Opcodes.ASM5);
+        classReader.accept(cn, 0);
+        is.close();
+        return cn;
+    }
+
+    public static MethodNode getMethodNode(
+            final ClassNode cn, final String name, final String desc) throws IOException {
+        for (final MethodNode mn : cn.methods) {
+            if (mn.name.equals(name) && mn.desc.equals(desc)) {
+                return mn;
+            }
+        }
+        throw new NoSuchElementException("Unknown method: " + name + desc);
+    }
+
+}
